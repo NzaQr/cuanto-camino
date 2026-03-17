4f8cd8daf19459f44154c645c8008af730c64103
Nazareno Quiroga <nnazarenoquiroga@gmail.com>
2026-03-17 10:58:01 -0300

    style: update map template

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -148,8 +148,8 @@ function MapView({
     >
       <ZoomControlToTopRight />
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
       />
       <ClickHandler onMapClick={onMapClick} />
       <FitBounds
